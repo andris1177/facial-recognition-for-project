a7bf1e34e9d6566e8a6aafa191b2ba3f6fb3a300
andris1177 <43418048+andris1177@users.noreply.github.com>
2023-01-02 00:09:54 +0100

    a

---
+++ local/main.py
@@ -0,0 +1,8 @@
+import cv2
+import face_recognition
+
+img = cv2.imread("images/image.png")
+rgb_img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+img_encoding = face_recognition.face_encodings(rgb_img)
+
+cv2.imshow("Img", img)
