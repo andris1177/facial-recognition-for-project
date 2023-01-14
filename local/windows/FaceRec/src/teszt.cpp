b48567762b105255dac63ef61bd35da7dd080548
andris1177 <43418048+andris1177@users.noreply.github.com>
2023-01-14 18:13:06 +0100

    windows

--- local/windows/FaceRec/src/teszt.cpp
+++ local/windows/FaceRec/src/teszt.cpp
@@ -1,7 +1,28 @@
-#include <opencv2/opencv.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
 #include <iostream>
 
 int main()
 {
-	std::string faces = "../web/images/"
+	
+	//d::string img = "../../../../../../../Users/Andris-PC/Pictures/Camera Roll/img.jpg";
+	
+	//d::string src = "../../../../../../../Users/Andris-PC/Pictures/Camera Roll/video.mp4";
+	
+
+	//webkamera
+	cv::VideoCapture cap(0);
+	cv::Mat img;
+
+	while (true)
+	{
+		cap.read(img);
+		cv::imshow("Image", img);
+		cv::waitKey(1);
+	}
+
+
+	return 0;
 }
+
