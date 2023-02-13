cbb876e916f0d0ca2f5f11372ed4087d0a793a7a
andris1177 <43418048+andris1177@users.noreply.github.com>
2023-02-13 21:23:41 +0100

    website

--- web/register.js
+++
@@ -1,9 +0,0 @@
-function validation() {  
-    var id = document.f1.user.value;  
-    var ps = document.f1.pass.value;
-    var sc = document.f1.spec.value; 
-    if(id.length === 0 || ps.length === 0 || sc.length === 0) {  
-        alert("All fields are required");  
-        return false;  
-    }  
-}
