39f2ba686f743e2bfa7147152c48132db6dee0e4
andris1177 <43418048+andris1177@users.noreply.github.com>
2023-02-17 00:55:30 +0100

    login

--- web/connect.php
+++ web/connect.php
@@ -1,11 +1,14 @@
-<?php      
-    $host = "localhost";  
-    $user = "root";  
-    $password = '';  
-    $db_name = "users";  
-      
-    $con = mysqli_connect($host, $user, $password, $db_name);  
-    if(mysqli_connect_errno()) {  
-        die("Failed to connect with MySQL: ". mysqli_connect_error());  
-    }  
-?>  
+<?php
+  $servername = "localhost";
+  $username = "root";
+  $password = "";
+  $dbname = "users";
+
+  // Create connection
+  $conn = new mysqli($servername, $username, $password, $dbname);
+
+  // Check connection
+  if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+  }
+?>
--- web/login/login.php
+++ web/login/login.php
@@ -1,7 +1,20 @@
 <?php
-$username = "";
-$password = "";
+include '../connect.php';
+ $username = $_POST['username'];
+ $password = $_POST['password'];
 
-if 
-
+ $sql = "SELECT * FROM users WHERE username = '$username'";
+ $result = mysqli_query($conn, $sql);
+ 
+ if (mysqli_num_rows($result) > 0) {
+     $row = mysqli_fetch_assoc($result);
+     if (password_verify($password, $row["password"])) {
+        header("Location: ../adminSite/mainSite.html");
+        exit();
+     } else {
+         // Rossz jelszó
+     }
+ } else {
+     // Rossz felhasználónév
+ }
 ?>
