677fd2c16b8eec0ca82f20197bb46e9428b6ab01
andris1177 <43418048+andris1177@users.noreply.github.com>
2023-02-17 14:11:20 +0100

    add faces

--- web/login/login.php
+++ web/login/login.php
@@ -3,18 +3,4 @@ include '../connect.php';
  $username = $_POST['username'];
  $password = $_POST['password'];
 
- $sql = "SELECT * FROM users WHERE username = '$username'";
- $result = mysqli_query($conn, $sql);
- 
- if (mysqli_num_rows($result) > 0) {
-     $row = mysqli_fetch_assoc($result);
-     if (password_verify($password, $row["password"])) {
-        header("Location: ../adminSite/mainSite.html");
-        exit();
-     } else {
-         // Rossz jelszó
-     }
- } else {
-     // Rossz felhasználónév
- }
 ?>
