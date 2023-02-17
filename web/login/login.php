bebfa69b7cac21c2f2e510f48ff0aaa46cb3454b
andris1177 <43418048+andris1177@users.noreply.github.com>
2023-02-17 15:23:26 +0100

    login

--- web/login/login.php
+++ web/login/login.php
@@ -3,4 +3,13 @@ include '../connect.php';
  $username = $_POST['username'];
  $password = $_POST['password'];
 
+ $sql = "SELECT * FROM users WHERE username = '$username' AND password = '$password'";
+ $result = mysqli_query($conn, $sql);
+
+ if (mysqli_num_rows($result) == 1) {
+    header("Location: ../adminSite/mainSite.html");
+    exit;
+ } else {
+    echo "Bad usernaem or password";
+ }
 ?>
