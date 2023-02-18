829e3dc79974e57037e01f758ee4e6455a7f8098
andris1177 <43418048+andris1177@users.noreply.github.com>
2023-02-18 23:19:55 +0100

    visual studio

--- local/windows/FaceRec/src/main.cpp
+++ local/windows/FaceRec/src/main.cpp
@@ -1,7 +1,27 @@
 #include <iostream>
-#include <opencv2/opencv.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/objdetect.hpp>
 
 int main()
 {
-	std::cout << "Hello world" << std::endl;
+	cv::VideoCapture video(0);
+	cv::CascadeClassifier facedetect;
+	cv::Mat img;
+	facedetect.load("C:/github local/facial-recognition-for-project/local/windows/FaceRec/src/cascades/haarcascades/haarcascade_frontalface_default.xml");
+	while (true)
+	{
+		video.read(img);
+		std::vector<cv::Rect> faces;
+		facedetect.detectMultiScale(img, faces, 1.3, 5);
+		for (int i = 0; i < faces.size(); i++)
+		{
+			cv::rectangle(img, faces[i].tl(), faces[i].br(), cv::Scalar(50, 50, 255), 3);
+		}
+		cv::imshow("Preview", img);
+		cv::waitKey(1);
+	}
+
+	return 0;
 }
