9c289614fad4597926b9334e9fd46eefacc67f3c
andris1177 <43418048+andris1177@users.noreply.github.com>
2023-05-25 14:17:45 +0200

    webpage

--- web/adminSite/takePicture/takePicture.js
+++ web/adminSite/takePicture/takePicture.js
@@ -2,25 +2,36 @@ Webcam.set({
     width: 250,
     height: 250,
     image_format: "jpeg",
-    jpeg_quality: 90,});
-
-Webcam.attach("#my_camera");
-
-function take_snapshot() {
-  for (let i = 1; i <= 50; i++) {
-      Webcam.snap(function (data_uri) {
+    jpeg_quality: 90,
+  });
+  
+  Webcam.attach("#my_camera");
+  
+  function take_snapshot() {
+    var i = 1;
+    capturePhoto();
+  
+    function capturePhoto() {
+      if (i <= 50) {
+        Webcam.snap(function (data_uri) {
           $(".image-tag").val(data_uri);
-          document.getElementById("results").innerHTML +=
-              '<img src="' + data_uri + '"/>';
-          
+          document.getElementById("results").innerHTML = ''; // Clear previous images
+          document.getElementById("results").innerHTML += '<img src="' + data_uri + '"/>';
+  
           $.ajax({
-              type: "POST",
-              url: "saveImage.php",
-              data: { image: data_uri, imagename: 'usergivenname' + i },
-              success: function (response) {
-                  console.log(response);
-              }
+            type: "POST",
+            url: "http://localhost/adminSite/takePicture/takePicture.php",
+            data: { image: data_uri, imagename: $('#imagename').val() + i },
+            success: function (response) {
+              console.log(response);
+            }
           });
-      });
+  
+          i++;
+          setTimeout(capturePhoto, 200); // Delay of 0.2 seconds (200 milliseconds)
+          console.log("ez egy kép!")
+        });
+      }
+    }
   }
-}
+  
--- web/adminSite/takePicture/takePicture.php
+++ web/adminSite/takePicture/takePicture.php
@@ -1,18 +1,18 @@
 <?php
 include "../../connect.php";
 
-for ($i = 1; $i <= 50; $i++) {
-    $img = $_POST['image'];
-    $imagename = $_POST['imagename'] . $i;
 
-    $image_data = str_replace('data:image/jpeg;base64,', '', $img);
+$img = $_POST['image'];
+$imagename = $_POST['imagename'] ;
 
-    $sql = "INSERT INTO képek (name, image) VALUES (?, ?)";
-    $stmt = $conn->prepare($sql);
+$image_data = str_replace('data:image/jpeg;base64,', '', $img);
 
-    $stmt->bind_param("ss", $imagename, $image_data);
-    $stmt->execute();
-}
+$sql = "INSERT INTO képek (name, image) VALUES (?, ?)";
+$stmt = $conn->prepare($sql);
+
+$stmt->bind_param("ss", $imagename, $image_data);
+$stmt->execute();
+
 
 header("Location: takePicture.html");
 exit; 
